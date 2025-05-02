73cd051388c950e1059a3f01270bebd07c838ed1
Rasool <rasoolol.77@gmail.com>
2025-05-02 15:18:31 +0330

    ignore lints

--- src/email/email.consumer.ts
+++ src/email/email.consumer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
